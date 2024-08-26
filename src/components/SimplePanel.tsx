bc07d08a24b4030504f27c32e759212eb920d598
Matt Sheppard <msheppard@beta.team>
2024-08-26 14:06:30 -0400

    pushed to mampersat git

--- src/components/SimplePanel.tsx
+++ src/components/SimplePanel.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from '@emotion/css';
-import { useStyles2, useTheme2 } from '@grafana/ui';
-import { PanelDataErrorView } from '@grafana/runtime';
+import { useStyles2} from '@grafana/ui';
 
-import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
 
 interface Props extends PanelProps<SimpleOptions> { }
